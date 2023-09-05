416d09e72fb84d724916cd4219913b555befb8da
Anthony <skydabutterrs+github@gmail.com>
2023-09-05 04:00:59 -0400

    match replays supported

---
+++ loguploader.js
@@ -0,0 +1,9 @@
+const { readdirSync, writeFileSync } = require('fs');
+let html = `\t\t\t\t<tr>\n\t\t\t\t\t<th>Game Chat Logs</th>\n\t\t\t\t\t<th>Game Replays</th>\n\t\t\t\t</tr>`;
+let replays = readdirSync('./replays')
+
+readdirSync('./uploadlogs').forEach((f, number = 0) => {
+    console.log(`${f}\n${replays[number]}`)
+    html = `${html}\n\t\t\t\t<tr>\n\t\t\t\t\t<td><a href="./logs/${f}">${f}</a></td>\n\t\t\t\t\t<td><a href="./replays/${replays[number]}" download>Download Replay</a></td>\n\t\t\t\t</tr>`
+    writeFileSync('./logs.html', html)
+});
