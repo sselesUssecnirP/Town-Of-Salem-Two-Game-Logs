3c105215d895ea8c237f4f7101138fa558049013
Anthony <skydabutterrs+github@gmail.com>
2023-09-28 10:28:20 -0400

    COLORS!

--- loguploader.js
+++ loguploader.js
@@ -6,7 +6,7 @@ readdirSync('./uploadlogs').forEach((f, number = 0) => {
     let playerName;
     let role;
     let username;
-    const regex = /<span><br>\[[0-9]{1,2}\] ([\s\S]*?) - <\/span><span style="color:#[0-9A-Fa-f]+"> ([\s\S]*?) <\/span>[\s\S]*?\(Username: ([\s\S]+?)\)<\/i><\/span>/g;
+    const regex = /<span><br>\[[0-9]{1,2}\] ([\s\S]*?) - <\/span>(<span style="color:#[0-9A-Fa-f]+"> [\s\S]*? <\/span>)[\s\S]*?\(Username: ([\s\S]+?)\)<\/i><\/span>/g;
 
     let match;
     while ((match = regex.exec(readFileSync(`./uploadlogs/${f}`, { encoding: 'utf-8', flags: 'r' }))) !== null) {
@@ -18,6 +18,6 @@ readdirSync('./uploadlogs').forEach((f, number = 0) => {
     }
 
     console.log(`${f}\n${replays[number]}`)
-    html = `${html}\n\t\t\t\t<tr>\n\t\t\t\t\t<td><a href="./uploadlogs/${f}">${f}</a></td>\n\t\t\t\t\t<td>${playerName || 'Anonymous'}</td>\n\t\t\t\t\t<td>${role || 'Anonymous'}</td>\n\t\t\t\t\t<td><a href="./replays/${replays[number]}" download>Download Replay</a></td>\n\t\t\t\t</tr>`
+    html = `${html}\n\t\t\t\t<tr>\n\t\t\t\t\t<td><a href="./uploadlogs/${f}">${f}</a></td>\n\t\t\t\t\t<td style="color:#fff">${playerName || '<span style="color:#fff">Anonymous</span>'}</td>\n\t\t\t\t\t<td>${role || '<span style="color:#fff">Anonymous</span>'}</td>\n\t\t\t\t\t<td><a href="./replays/${replays[number]}" download>Download Replay</a></td>\n\t\t\t\t</tr>`
     writeFileSync('./logs.html', html)
 });
